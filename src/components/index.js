5ab57cb22d1e578c3217cc8712a8fa485a1b1fbd
Darshan1606 <darshantarsariya17@gmail.com>
2023-01-31 16:14:43 +0530

    navbar done

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar/Navbar.jsx'
+export { default as Feed } from './Feed/Feed.jsx'
+export { default as ChannelDetail } from './ChannelDetail/ChannelDetail.jsx'
+export { default as VideoDetail } from './VideoDetail/VideoDetail.jsx'
+export { default as SearchFeed } from './SearchFeed/SearchFeed.jsx'
+export { default as SearchBar } from './SearchBar/SearchBar.jsx'
