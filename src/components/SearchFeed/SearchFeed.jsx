583ad423594416daa8d8fbd042b59aff71578e01
Darshan1606 <darshantarsariya17@gmail.com>
2023-02-01 14:17:48 +0530

    search feed work success

--- src/components/SearchFeed/SearchFeed.jsx
+++ src/components/SearchFeed/SearchFeed.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Typography, Box } from '@mui/material'
+import { useParams } from 'react-router-dom'
+
+import { fetchAPI } from '../../utils/fetchAPI'
+import { Videos } from '../../components'
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+  }, []);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} minHeight="95vh">
+      <Typography variant="h4" fontWeight={900} color="white" mb={3} ml={{ sm: "100px" }}>
+        Search Results for <span style={{ color: "#2caee2" }}>{searchTerm}</span> videos
+      </Typography>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        {<Videos videos={videos} />}
+      </Box>
+    </Box>
   )
 }
 
